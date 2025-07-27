65fb662c1c73d99d41ed8d2e16f7c3dc6550f734
user <user@MacBook-Air-Anastasia.local>
2025-07-27 17:21:44 +0500

    start zero

--- app/models/user.py
+++ app/models/user.py
@@ -1,77 +1,44 @@
-from sqlmodel import Field, Relationship, SQLModel
-from pydantic import EmailStr
 import uuid
+from sqlmodel import SQLModel, Field, Relationship
+from pydantic import EmailStr
 
 from item import Item
 
-# Shared properties
+
 class UserBase(SQLModel):
     email: EmailStr = Field(unique=True, index=True, max_length=255)
     is_active: bool = True
     is_superuser: bool = False
     full_name: str | None = Field(default=None, max_length=255)
 
-
-# Properties to receive via API on creation
 class UserCreate(UserBase):
     password: str = Field(min_length=8, max_length=40)
-
 
 class UserRegister(SQLModel):
     email: EmailStr = Field(max_length=255)
     password: str = Field(min_length=8, max_length=40)
     full_name: str | None = Field(default=None, max_length=255)
 
-
-# Properties to receive via API on update, all are optional
 class UserUpdate(UserBase):
-    email: EmailStr | None = Field(default=None, max_length=255)  # type: ignore
-    password: str | None = Field(default=None, min_length=8, max_length=40)
-
+    email: EmailStr | None = Field(default=None, max_length=255)
+    password: str | None = Field(default=None, min_length=8, max_length=255)
 
 class UserUpdateMe(SQLModel):
     full_name: str | None = Field(default=None, max_length=255)
     email: EmailStr | None = Field(default=None, max_length=255)
 
-
 class UpdatePassword(SQLModel):
-    current_password: str = Field(min_length=8, max_length=40)
-    new_password: str = Field(min_length=8, max_length=40)
+    current_password: str | None = Field(min_length=8, max_length=255)
+    new_password: str | None = Field(min_length=8, max_length=255)
 
-
-# Database model, database table inferred from class name
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
-    items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
+    items: list['Item'] = Relationship(back_populates='owner', cascade_delete=True)
 
-
-# Properties to return via API, id is always required
 class UserPublic(UserBase):
     id: uuid.UUID
-
 
 class UsersPublic(SQLModel):
     data: list[UserPublic]
     count: int
-
-
-# Generic message
-class Message(SQLModel):
-    message: str
-
-
-# JSON payload containing access token
-class Token(SQLModel):
-    access_token: str
-    token_type: str = "bearer"
-
-
-# Contents of JWT token
-class TokenPayload(SQLModel):
-    sub: str | None = None
-
-
-class NewPassword(SQLModel):
-    token: str
-    new_password: str = Field(min_length=8, max_length=40)
